55e41ce1a53004b310359a93923fbc3a9d0d91aa
lmelikishvili <levanimelikishvili@gmail.com>
2023-11-28 17:40:56 +0400

    base_setup

---
+++ app/src/main/java/com/example/homework13/editFields/FieldFragment.kt
@@ -0,0 +1,15 @@
+package com.example.homework13.editFields
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homework13.BaseFragment
+import com.example.homework13.R
+import com.example.homework13.databinding.FragmentFieldBinding
+
+class FieldFragment : BaseFragment<FragmentFieldBinding>(FragmentFieldBinding::inflate) {
+
+
+}
